741c94c25894532c253d16f5a796c15833cf6b2c
Abhi Shek <polampallyabhishek890@gmail.com>
2022-06-27 19:35:38 +0530

    First Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+      <div className="logo">
+        <span>REACT THREE FIBER</span>
+      </div>
+      <div className="episode">
+        <span>EP. 1</span>
+      </div>
+    </header>
+  )
+}
